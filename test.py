dab874f0182145d05d82432b7fafc26c6f14bfb5
R1cRamos <rramosz@gmail.com>
2024-08-27 16:27:13 +0200

    Create test.py
    Test code created and pasted

---
+++ test.py
@@ -0,0 +1,10 @@
+def suma(a, b):
+    return a + b
+
+def test_suma():
+    assert suma(2, 3) == 5
+    assert suma(-1, 1) == 0
+
+if __name__ == "__main__":
+    test_suma()
+    print("¡Todas las pruebas pasaron!")
